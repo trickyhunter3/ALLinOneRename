34c6c6ec29127d6f4adc24ec3f57444b532eacd0
denisplay <workingdenniss@gmail.com>
2021-02-15 19:44:59 +0200

    Add project files.

---
+++ ALLinOneRename/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace ALLinOneRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRenameVOne = new System.Windows.Forms.Button();
+            this.BtnSubtractName = new System.Windows.Forms.Button();
+            this.BtnRenameVTwo = new System.Windows.Forms.Button();
+            this.BtnCheckFiles = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RtbRenamedText = new System.Windows.Forms.RichTextBox();
+            this.TbxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnRenameVOne
+            // 
+            this.BtnRenameVOne.Location = new System.Drawing.Point(52, 118);
+            this.BtnRenameVOne.Name = "BtnRenameVOne";
+            this.BtnRenameVOne.Size = new System.Drawing.Size(75, 23);
+            this.BtnRenameVOne.TabIndex = 0;
+            this.BtnRenameVOne.Text = "Change V1";
+            this.BtnRenameVOne.UseVisualStyleBackColor = true;
+            this.BtnRenameVOne.Click += new System.EventHandler(this.BtnRenameVOne_Click);
+            // 
+            // BtnSubtractName
+            // 
+            this.BtnSubtractName.Location = new System.Drawing.Point(52, 231);
+            this.BtnSubtractName.Name = "BtnSubtractName";
+            this.BtnSubtractName.Size = new System.Drawing.Size(75, 23);
+            this.BtnSubtractName.TabIndex = 1;
+            this.BtnSubtractName.Text = "Subtract Name";
+            this.BtnSubtractName.UseVisualStyleBackColor = true;
+            // 
+            // BtnRenameVTwo
+            // 
+            this.BtnRenameVTwo.Location = new System.Drawing.Point(585, 124);
+            this.BtnRenameVTwo.Name = "BtnRenameVTwo";
+            this.BtnRenameVTwo.Size = new System.Drawing.Size(75, 23);
+            this.BtnRenameVTwo.TabIndex = 2;
+            this.BtnRenameVTwo.Text = "Change V2";
+            this.BtnRenameVTwo.UseVisualStyleBackColor = true;
+            this.BtnRenameVTwo.Click += new System.EventHandler(this.BtnRenameVTwo_Click);
+            // 
+            // BtnCheckFiles
+            // 
+            this.BtnCheckFiles.Location = new System.Drawing.Point(585, 231);
+            this.BtnCheckFiles.Name = "BtnCheckFiles";
+            this.BtnCheckFiles.Size = new System.Drawing.Size(75, 23);
+            this.BtnCheckFiles.TabIndex = 3;
+            this.BtnCheckFiles.Text = "Check Files";
+            this.BtnCheckFiles.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rename files to numbers";
+            // 
+            // RtbRenamedText
+            // 
+            this.RtbRenamedText.Location = new System.Drawing.Point(190, 81);
+            this.RtbRenamedText.Name = "RtbRenamedText";
+            this.RtbRenamedText.ReadOnly = true;
+            this.RtbRenamedText.Size = new System.Drawing.Size(357, 357);
+            this.RtbRenamedText.TabIndex = 5;
+            this.RtbRenamedText.Text = "";
+            // 
+            // TbxPath
+            // 
+            this.TbxPath.Location = new System.Drawing.Point(190, 41);
+            this.TbxPath.Name = "TbxPath";
+            this.TbxPath.Size = new System.Drawing.Size(357, 20);
+            this.TbxPath.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TbxPath);
+            this.Controls.Add(this.RtbRenamedText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCheckFiles);
+            this.Controls.Add(this.BtnRenameVTwo);
+            this.Controls.Add(this.BtnSubtractName);
+            this.Controls.Add(this.BtnRenameVOne);
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRenameVOne;
+        private System.Windows.Forms.Button BtnSubtractName;
+        private System.Windows.Forms.Button BtnRenameVTwo;
+        private System.Windows.Forms.Button BtnCheckFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox RtbRenamedText;
+        private System.Windows.Forms.TextBox TbxPath;
+    }
+}
+
---
+++ ALLinOneRename/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ALLinOneRename
+{
+    public partial class Form1 : Form
+    {
+        int[] numFilter;
+        bool needFilter = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRenameVOne_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRenameVTwo_Click(object sender, EventArgs e)
+        {
+            string usersPath = TbxPath.Text;
+
+            //get the directory info files and check if there is a path
+            DirectoryInfo directoryInfo = new DirectoryInfo(usersPath);
+            FileInfo[] infos = directoryInfo.GetFiles();
+            
+            foreach (FileInfo fileInfo in infos)
+            {
+                try
+                {
+                    if (fileInfo.Name == "desktop.ini" || fileInfo.Name == "icon.ico")
+                        goto END;
+
+                    string filterNumbers = null; // numbers that will be filtered
+
+                    string fileType;
+
+                    string numberSide = "";
+
+                    string seriesName = fileInfo.Directory.Parent.Name;
+                    string newPath;
+                    string finalName;
+                    string seasonNum;
+
+                    int numberFromTheString;
+                    fileType = '.' + fileInfo.Name.Split('.')[fileInfo.Name.Split('.').Length - 1];
+
+                    string seasonWithNumber = fileInfo.Directory.Name;
+                    string[] seasonAndNumberSplited = seasonWithNumber.Split(' ');
+
+                    if (seasonAndNumberSplited[0].ToLower() == "season")
+                    {
+                        numberFromTheString = GetNumberOutOfString(fileInfo.Name, fileType, numberSide);
+                        seasonNum = seasonAndNumberSplited[1];
+
+                        finalName = CreateFinalName(numberFromTheString, seasonNum, seriesName);
+
+                        // File.Move(fileInfo.FullName, usersPath + finalName + fileType);
+
+                        Console.WriteLine("{0} Complete \\\\ {1}", numberFromTheString.ToString(), fileInfo.Name);
+                    }
+
+                    else
+                    {
+                        numberFromTheString = GetNumberOutOfString(fileInfo.Name, fileType, numberSide);
+                    }
+                }
+                catch (IOException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(fileInfo.Name + " Already exist");
+                    Console.ResetColor();
+                }
+            END:;
+            }
+
+            string CreateFinalName(int numberFromTheString, string seasonNum, string seriesName)
+            {
+                string helperName;
+                string episodeHelper = "E";
+
+                if (numberFromTheString / 10 < 1)
+                    episodeHelper = "E0";
+
+                helperName = CreateHelperName(seasonNum, numberFromTheString, episodeHelper);
+
+                return seriesName + " - " + helperName;
+            }
+
+            string CreateHelperName(string seasonNum, int numberFromTheString, string episodeHelper)
+            {
+                if (seasonNum == "00" || seasonNum.ToLower() == "specials")
+                    return "S" + seasonNum + episodeHelper + numberFromTheString.ToString();
+                else if (Convert.ToInt32(seasonNum) < 9)
+                    return "S0" + seasonNum + episodeHelper + numberFromTheString.ToString();
+                else
+                    return "S" + seasonNum + episodeHelper + numberFromTheString.ToString();
+            }
+
+            int GetNumberOutOfString(string File_name, string file_type, string Side)
+            {
+                // j is current index of the file_name 
+                int converted = 0;
+                //if we find a number that is episode then i++ happen so we save the episode number and 
+                //on the next run when it find a season number or resoulution number it will go to 0 on the next int not on the
+                //episode number itself
+                int numbers_together = 0;
+                //when he find number he start to count so that it won't check if statment IF he is not at least 1 number
+                int number_holder = -1;
+                //hold a number if it's the only number then 
+                string numbers = null;
+                for (int j = 0; j < File_name.Length; j++)
+                {
+                    switch (File_name[j])
+                    {
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            numbers_together++;
+                            numbers += File_name[j];
+                            //start recording the numbers if they are found
+                            break;
+                        default:
+                            if (numbers_together != 0)
+                            {
+                                if (Side == "f")
+                                    return Convert.ToInt32(numbers);
+                                if (numbers + file_type == File_name)
+                                {
+                                    converted = Convert.ToInt32(numbers);
+                                    number_holder = 0;
+                                    goto END;
+                                    //if file is just a number then returns that number
+                                }
+                                if (numbers == "0")
+                                    number_holder = 0;
+                                if (needFilter)
+                                {
+                                    for (int i = 0; i < numFilter.Length; i++)
+                                    {
+                                        if (numFilter[i] == Convert.ToInt32(numbers))
+                                        {
+                                            if (number_holder == 0)
+                                            {
+                                                goto END;
+                                            }
+                                            number_holder = Convert.ToInt32(numbers);
+                                            goto END;
+                                        }
+                                    }
+                                }
+                                switch (numbers)
+                                {
+                                    case "1":
+                                    case "2":
+                                    case "3":
+                                    case "4":
+                                    case "5":
+                                    case "6":
+                                    case "7":
+                                    case "8":
+                                    case "9":
+                                    case "640":
+                                    case "720":
+                                    case "1080":
+                                    case "1920":
+                                    case "2160":
+                                    case "2010":
+                                        if (number_holder == 0)
+                                        {
+                                            goto END;
+                                        }
+                                        number_holder = Convert.ToInt32(numbers);
+                                        goto END;
+                                }
+                                converted = Convert.ToInt32(numbers);
+                            END:;
+                                numbers = null;
+                            }
+                            numbers_together = 0;
+                            break;
+                    }
+                }
+                if (converted + number_holder == number_holder)
+                    return number_holder;
+                //converted + num = num that means that the season or resolution filter worked but was not necessery
+                return converted;
+            }
+
+        }
+
+    }
+}
